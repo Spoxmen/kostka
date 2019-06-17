35998563eff2b9a3067bcbc596bfe67179b56e58
Spoxmen <spoxmen@gmail.com>
2019-06-17 02:26:54 +0200

    naprawianie

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var port = 3000
+
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,5 +22,35 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+app.post('/', function (req, res, next) {
+    var dane = req.body
+    console.log(dane.ST);
+    console.log(dane.IK);
+
+    var ilosc_sukcesow=0;
+    var ilosc_przerzutow=0;
+
+    for (var i=0; dane.IK > i; i++){
+        var wynik =  1 + Math.floor(Math.random() * 10);
+            if(wynik>=dane.ST){
+                ilosc_sukcesow=ilosc_sukcesow+1
+            if(wynik==10){
+                ilosc_rzutow++
+                ilosc_przerzutow++
+            }
+            }
+    }
+    console.log('Ilość sukcesów to ' + ilosc_sukcesow)
+    console.log('Ilość przerzutów to ' + ilosc_przerzutow)
+    var response = 'Ilość sukcesów to ' + ilosc_sukcesow
+    res.send(response)
+})
+
 module.exports = app;
+module.exports = app;
+
+var PORT = process.env.PORT || 3000;
+var server=app.listen(PORT,function() {});
+app.listen(PORT, () => console.log(`Example app listening on port ${port}!`))
